934968fac61a9027feb923b38bdc39110a5bae93
Bakeneko <monopolecat@gmail.com>
2023-06-14 17:33:47 +0900

    prettierの適応

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import {useState} from "react"
+import { useState } from "react";
 import "./App.css";
-import {PrefectureItem} from "./index"
+import { PrefectureItem } from "./index";
 
 const App = () => {
-  const [prefecture, setPrefecture] = useState(true);
-  const PrefectureChecked = (event:any) => {
-    setPrefecture(event.target.checked);
-  }
-  return (
-    <>
-      <h1>YumemiAssignment</h1>
-      <PrefectureItem prefecture="東京都" onChange={PrefectureChecked} />
-    </>
-  );
-}
+    const [prefecture, setPrefecture] = useState(true);
+    const PrefectureChecked = (event: any) => {
+        setPrefecture(event.target.checked);
+    };
+    return (
+        <>
+            <h1>YumemiAssignment</h1>
+            <PrefectureItem prefecture="東京都" onChange={PrefectureChecked} />
+        </>
+    );
+};
 
 export default App;
