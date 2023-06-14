4ed100ef7010216cdb261bfef6d94281506b04dd
Bakeneko <monopolecat@gmail.com>
2023-06-14 22:42:01 +0900

    GetPrefecturesJsonの作成に伴う、エンドポイントとApp.tsxの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import {useState} from "react"
+import { useEffect, useState } from "react";
 import "./App.css";
-import {PrefectureItem} from "./index"
+import { PrefectureItem, GetPrefecturesJson } from "./index";
 
 const App = () => {
-  const [prefecture, setPrefecture] = useState(true);
-  const PrefectureChecked = (event:any) => {
-    setPrefecture(event.target.checked);
-  }
-  return (
-    <>
-      <h1>YumemiAssignment</h1>
-      <PrefectureItem prefecture="東京都" onChange={PrefectureChecked} />
-    </>
-  );
-}
+    const [prefecturesJson, setPrefecturesJson] = useState("");
+    const prefecturesUpdate = (data: string) => {
+        setPrefecturesJson(data);
+    };
+    useEffect(() => {
+        //初回実行で都道府県名を取得
+        GetPrefecturesJson(prefecturesUpdate);
+    }, []);
+    return (
+        <>
+            <h1>YumemiAssignment</h1>
+        </>
+    );
+};
 
 export default App;
