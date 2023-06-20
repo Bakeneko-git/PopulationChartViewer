5dee55122c858dba64eaa0a06c01b1c43f8b8437
Bakeneko <monopolecat@gmail.com>
2023-06-20 23:59:04 +0900

    importのPrefecturesListをスペルミスしていたので修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react";
 import "./App.css";
-import { PrefectureList, GetPrefecturesJson } from "./index";
+import { PrefecturesList, GetPrefecturesJson } from "./index";
 
 const App = () => {
     const [prefecturesJson, setPrefecturesJson] = useState("");
