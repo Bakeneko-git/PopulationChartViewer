edfbef9daabfc75f1618f286646ba6f969c4c6f3
Bakeneko <monopolecat@gmail.com>
2023-06-26 21:20:28 +0900

    Updates

--- src/components/GetPrefecturesJson.tsx
+++
@@ -1,9 +0,0 @@
-import GetResasJson from "./GetResasJson";
-
-const GetPrefecturesJson = async () => {
-    const requrl = "api/v1/prefectures";
-    const result = await GetResasJson(requrl);
-    return result;
-};
-
-export default GetPrefecturesJson;
