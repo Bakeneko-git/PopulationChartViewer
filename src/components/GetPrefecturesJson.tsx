4905770666316f413b8a46dfef5a58ceb9050870
Bakeneko <monopolecat@gmail.com>
2023-06-26 15:09:21 +0900

    GetResasJsonを利用するようにアップデート

--- src/components/GetPrefecturesJson.tsx
+++ src/components/GetPrefecturesJson.tsx
@@ -1,16 +1,8 @@
+import GetResasJson from "./GetResasJson";
+
 const GetPrefecturesJson = async () => {
-    const apikey = process.env.REACT_APP_RESAS_API_KEY;
-    const headers: Record<string, string> = {
-        "X-API-KEY": apikey !== undefined ? apikey : "",
-        "Content-Type": "application/json",
-    };
-    const requestOptions: RequestInit = {
-        headers: headers,
-    };
-    const result = await fetch(
-        "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-        requestOptions
-    ).then((response) => response.json());
+    const requrl = "api/v1/prefectures";
+    const result = await GetResasJson(requrl);
     return result;
 };
 
